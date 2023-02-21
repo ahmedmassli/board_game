dee06c404624ea9e6377b4e1fe42a49951257eba
ahmedmassli <ahmedmassli@hotmail.co.uk>
2023-02-21 12:07:11 +0000

    progress 1 task4

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -55,4 +55,47 @@ describe("app", () => {
   });
 });
 
-("testpush");
+describe("get /api/reviews", () => {
+  test("200: GET /api/reviews responds with reviews data", () => {
+    return request(app)
+      .get("/api/reviews")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.revData.length).toBe(13);
+      });
+  });
+  describe("get /api/reviews", () => {
+    test("200: GET /api/reviews responds with reviews data", () => {
+      return request(app)
+        .get("/api/reviews")
+        .expect(200)
+        .then(({ body }) => {
+          expect(body.revData[0]).toEqual(
+            expect.objectContaining({
+              owner: expect.any(String),
+              title: expect.any(String),
+              review_id: expect.any(Number),
+              category: expect.any(String),
+              review_img_url: expect.any(String),
+              created_at: expect.any(String),
+              votes: expect.any(Number),
+              designer: expect.any(String),
+              comment_count: expect.any(Number),
+            })
+          );
+        });
+    });
+    test("200: GET /api/reviews sorts date by descending order.", () => {
+      return request(app)
+        .get("/api/reviews")
+        .expect(200)
+        .then(({ body }) => {
+          const copyBody = [...body.revData];
+          const sortedReviews = copyBody.sort((reviewA, reviewB) => {
+            return reviewB.date - reviewA.date;
+          });
+          expect(body.revData).toEqual(sortedReviews);
+        });
+    });
+  });
+});
--- controllers.js
+++ controllers.js
@@ -1,10 +1,25 @@
-const fetchCategories = require("./models");
+const { fetchCategories, fetchReviews } = require("./models");
 
-function getCategories(request, response) {
-  fetchCategories().then((catogData) => {
-    const obj = { catogData: catogData };
-    response.send(obj);
-  });
+function getCategories(request, response, next) {
+  fetchCategories()
+    .then((catogData) => {
+      const obj = { catogData: catogData };
+      response.send(obj);
+    })
+    .catch((err) => {
+      next(err);
+    });
 }
 
-module.exports = getCategories;
+function getReviews(request, response, next) {
+  fetchReviews()
+    .then((revData) => {
+      const obj = { revData: revData };
+      response.send(obj);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+module.exports = { getCategories, getReviews };
