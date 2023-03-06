edeaffea44426889b6f46490c7c76d84ddd93610
ahmedmassli <ahmedmassli@hotmail.co.uk>
2023-03-06 16:34:42 +0000

    progress20

--- models.js
+++ models.js
@@ -127,6 +127,26 @@ function fetchUsers() {
     });
 }
 
+// function fetchReviewsByQuery(review_id) {
+//   let queryString = `
+//         SELECT reviews.owner, reviews.title, reviews.review_id, reviews.review_img_url, reviews.category, reviews.created_at,reviews.votes,reviews.designer,reviews.review_body
+//         FROM reviews
+//         `;
+//   const queryParams = [];
+
+//   if (review_id !== undefined) {
+//     queryString += "WHERE review_id=$1;";
+//     queryParams.push(review_id);
+//   }
+//   return db.query(queryString, queryParams).then((result) => {
+//     const revs = result.rows;
+//     if (result.rowCount === 0) {
+//       return Promise.reject("review_id not found");
+//     }
+//     return revs;
+//   });
+// }
+
 module.exports = {
   fetchCategories,
   fetchReviews,
